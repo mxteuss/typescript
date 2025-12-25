2ba7ab0289885e4fe7e6955113bd49f9fe056be5
mxteuss <ovagabundant@gmail.com>
2025-12-25 14:01:36 -0300

    script

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,12 @@
 const btn = document.getElementById('btnEnviar');
 btn.addEventListener("click", () => {
     const nome = document.getElementById('name');
-<<<<<<< HEAD
     const contrato = document.getElementById('contrato');
-    if (contrato.value.toUpperCase() !== "Efetivo" && contrato.value.toUpperCase() !== "Terceirizado") {
+    if (contrato.value.toUpperCase() !== "EFETIVO" && contrato.value.toUpperCase() !== "TERCEIRIZADO") {
         alert("Tipo de contrato inválido! Use 'Efetivo' ou 'Terceirizado'.");
     }
     else {
         alert(`Funcionário ${nome.value} cadastrado com sucesso!`);
     }
-=======
-    const salario = document.getElementById('salario');
-    const contrato = document.getElementById('contrato');
-    alert(`Funcionário ${nome.value} cadastrado com sucesso!`);
->>>>>>> 619bd9a55e71436efbc5d6a2390cdaa0d074066b
 });
 export {};
