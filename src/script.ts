59538f569baa70af0472e50cb039198dd82a4086
mxteuss <ovagabundant@gmail.com>
2025-12-25 13:48:39 -0300

    feat: condição no contrato

--- src/script.ts
+++ src/script.ts
@@ -3,9 +3,12 @@ const btn = document.getElementById('btnEnviar') as HTMLInputElement;
 
 btn.addEventListener("click", () => {
     const nome = document.getElementById('name') as HTMLInputElement;
-    const salario = document.getElementById('salario') as HTMLInputElement;
     const contrato = document.getElementById('contrato') as HTMLInputElement;
 
+    if (contrato.value.toUpperCase() !== "Efetivo" && contrato.value.toUpperCase() !== "Terceirizado") {
+        alert("Tipo de contrato inválido! Use 'Efetivo' ou 'Terceirizado'.");
+    }   
+    else{
     alert(`Funcionário ${nome.value} cadastrado com sucesso!`)
-    
+    }
 });
