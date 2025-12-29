a082563bd60c8b9a342a1f4249c2355862f2e1df
mxteuss <ovagabundant@gmail.com>
2025-12-28 21:28:18 -0300

    feat: entidades

---
+++ src/model/employee.ts
@@ -0,0 +1,5 @@
+export interface Employee {
+    name: string;
+    contractType: string;
+    salary: number; 
+}
---
+++ src/model/validate.ts
@@ -0,0 +1,5 @@
+export interface Errors {
+    name?: string;
+    contractType?: string;
+    salary?: string;
+}
